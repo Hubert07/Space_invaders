b0c8eae1b14fc7179d8e6b48434d1b3779f7e84b
Hubert07 <gajewski.hubert95@gmail.com>
2018-04-03 21:21:57 +0200

    grawitacja w p5

--- js/app_p5.js
+++ js/app_p5.js
@@ -1,7 +1,6 @@
-// kod projektu [wpisz nazwę]
+// kod projektu [Flappy bird]
 
 var bird;
-
 
 function setup() {
   // put setup code here
@@ -16,13 +15,14 @@ background(0);
 bird.update();
 bird.show();
 
-
+  //ptak
 }
 function Bird() {
   this.y = width/2;
   this.x = 70;
 
-  this.gravity = 1;
+  this.gravity = 1/7;
+  this.gravitySpeed = 0;
   this.velocity = 0;
 
   this.show = function() {
@@ -30,7 +30,8 @@ function Bird() {
     ellipse(this.x, this.y, 20, 20);
   }
 this.update = function() {
+  this.gravitySpeed += this.gravity;
   this.veliocity += this.gravity;
-  this.y += this.velocity;
+  this.y += this.velocity + this.gravitySpeed
 }
 }
