d583d7def0628f5885a52d98061957cf4e4659a1
Hubert07 <gajewski.hubert95@gmail.com>
2018-04-09 18:26:40 +0200

    zmiana ptaka

--- js/app_p5.js
+++ js/app_p5.js
@@ -5,10 +5,16 @@ var img;
 
 function setup() {
   // put setup code here
-var cnv = createCanvas(800, 500, WEBGL);
-var x = (windowWidth - width) / 2;
-  cnv.position(x, 75);
-  cnv.parent('canvas1')
+
+
+// var cnv = createCanvas(800, 500, WEBGL);
+// var canvasx = (windowWidth - width) / 2;
+//   cnv.position(canvasx, 75);
+//   cnv.parent('canvas1')
+var canvasX = (windowWidth - width) /4;
+var canvasY = 100;
+var cnv = createCanvas(800, 500);
+cnv.position(canvasX, canvasY);
 
 bird = new Bird();
 
@@ -24,19 +30,28 @@ bird.show();
   //ptak
 }
 function Bird() {
-  this.y = 150;
-  this.x = (width) - 650;
+  this.y = height/5;
+  this.x = width/6;
 
-  this.gravity = 1/9;
+  this.gravity = 1/5;
   this.gravitySpeed = 0;
   this.velocity = 0;
 
   this.show = function() {
-    texture(img);
+    fill(255);
+   ellipse(this.x, this.y, 32, 32);
+
+    // texture(img);
   }
 this.update = function() {
   this.gravitySpeed += this.gravity;
   this.veliocity += this.gravity;
   this.y += this.velocity + this.gravitySpeed
+
+  if (this.y > height) {
+    this.y = height;
+    this.velocity = 0;
+  }
 }
+
 }
